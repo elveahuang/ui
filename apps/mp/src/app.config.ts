1840f1ebf367dc96b860600778a35d5d63a1c4c9
Elvea Huang <elvea.huang@gmail.com>
2025-05-24 12:13:00 +0800

    Initial commit

---
+++ apps/mp/src/app.config.ts
@@ -0,0 +1,9 @@
+export default {
+    pages: ['pages/index/index'],
+    window: {
+        backgroundTextStyle: 'light',
+        navigationBarBackgroundColor: '#FFF',
+        navigationBarTitleText: 'WeChat',
+        navigationBarTextStyle: 'black',
+    },
+};
