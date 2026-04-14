a781cf148e8f725e0e1c750fddf4d2f11457285f
Elvea Huang <elvea.huang@gmail.com>
2026-04-15 00:21:46 +0800

    初始文本编辑器

--- apps/server/app/pages/showcase/index.vue
+++ apps/server/app/pages/showcase/index.vue
@@ -1,5 +1,11 @@
 <template>
-    <Suspense> </Suspense>
+    <Suspense>
+        <ul>
+            <li><NuxtLink to="/showcase/chat">Chat</NuxtLink></li>
+            <li><NuxtLink to="/showcase/comark">Comark</NuxtLink></li>
+            <li><NuxtLink to="/showcase/tiptap">Tiptap</NuxtLink></li>
+        </ul>
+    </Suspense>
 </template>
 
 <script setup lang="ts"></script>
