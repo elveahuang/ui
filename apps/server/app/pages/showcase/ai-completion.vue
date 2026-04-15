37e6c925c5e7bb5b76a1431bbd521c1483e5cd48
Elvea Huang <elvea.huang@gmail.com>
2026-04-15 19:43:35 +0800

    AI Chat & Completion

---
+++ apps/server/app/pages/showcase/ai-completion.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { useCompletion } from '@ai-sdk/vue';
+
+const { completion, input, handleSubmit, data } = useCompletion();
+</script>
+
+<template>
+    <div class="stretch mx-auto flex w-full max-w-md flex-col py-24">
+        <h4 class="pb-4 text-xl font-bold text-gray-900 md:text-xl">useCompletion Example</h4>
+        <pre>{{ data != null ? JSON.stringify(data, null, 2) : '' }}</pre>
+        {{ completion }}
+        <form @submit="handleSubmit">
+            <input class="fixed bottom-0 mb-8 w-full max-w-md rounded border border-gray-300 p-2 shadow-xl" v-model="input" placeholder="Say something..." />
+        </form>
+    </div>
+</template>
--- apps/server/server/api/completion.ts
+++ apps/server/server/api/completion.ts
@@ -1,16 +1,15 @@
-import { createDeepSeek, type DeepSeekProvider } from '@ai-sdk/deepseek';
-import { convertToModelMessages, streamText } from 'ai';
+import { createDeepSeek, DeepSeekProvider } from '@ai-sdk/deepseek';
+import { streamText } from 'ai';
 
 export default defineLazyEventHandler(async () => {
     const deepSeek: DeepSeekProvider = createDeepSeek({
         apiKey: useRuntimeConfig().deepseekApiKey,
     });
     return defineEventHandler(async (event: any) => {
-        const { messages } = await readBody(event);
-        console.log('messages', messages);
+        const { prompt } = await readBody(event);
         const result = streamText({
             model: deepSeek('deepseek-chat'),
-            messages: await convertToModelMessages(messages),
+            prompt,
         });
         return result.toUIMessageStreamResponse();
     });
